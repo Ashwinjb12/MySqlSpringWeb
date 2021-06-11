714dfcf6ddb89fbd3b9a64ccee013d93a5cad406
ashwin <ashwinjb2@gmail.com>
2021-06-11 12:52:11 +0530

    first commit

---
+++ src/main/java/com/ashwin/SpringWebMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.ashwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMySqlApplication.class, args);
+	}
+
+}
